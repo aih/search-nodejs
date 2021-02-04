cc2d70c26b674e4c42a7c07aef932de7eb66e47c
Alex <tictak211@gmail.com>
2021-02-04 18:54:39 +0200

    feat(search)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import { ElasticService } from './elastic/elastic.service';
+import { ElasticsearchService } from '@nestjs/elasticsearch';
+import { builtinModules } from 'module';
 
 @Controller()
 export class AppController {
-  constructor(private readonly elasticService: ElasticService) {}
+  constructor(private readonly elasticService: ElasticsearchService) {}
 
-  @Get('/search?:query')
-  search(@Query('q') query: string) {
-    return query;
+  // https://coralogix.com/log-analytics-blog/42-elasticsearch-query-examples-hands-on-tutorial/
+
+  // https://wanago.io/2020/09/07/api-nestjs-elasticsearch/
+
+  @Get('/search')
+  async search(@Query('q') text: string) {
+    const { body } = await this.elasticService.search({
+      body: {
+        query: {
+          multi_match: {
+            query: text,
+            fields: ['heading', 'text', 'xml'],
+          },
+        },
+      },
+    });
+
+    return body.hits;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { ElasticService } from './elastic/elastic.service';
 import { ElasticsearchModule } from '@nestjs/elasticsearch';
 import { ConfigModule } from '@nestjs/config';
 import config from './config/app.config';
@@ -12,6 +11,6 @@ import { ElasticConnectionService } from './elastic/elastic-connect/elastic-conn
     ElasticsearchModule.registerAsync({ useClass: ElasticConnectionService }),
   ],
   controllers: [AppController],
-  providers: [ElasticService, ElasticConnectionService],
+  providers: [ElasticConnectionService],
 })
 export class AppModule {}
